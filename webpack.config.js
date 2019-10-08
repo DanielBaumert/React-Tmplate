088518408ce5f654cac9e5d1ec4797855809399a
Daniel Baumert <DBaumertffg@web.de>
2019-10-08 15:38:21 +0200

    Include sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = { 
     entry: './src/index.js',
@@ -10,6 +11,7 @@ module.exports = {
         filename: "bundle.js"
     },
     mode: 'production',
+    devtool: 'inline-source-map',
     module: {
         rules: [
             { 
@@ -19,15 +21,29 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env', "@babel/preset-react"]
-                    }
+                    },
                 }
+            },{ 
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
             }
         ]
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', "index.html")
-        })
+            template: path.resolve(__dirname, 'src', "index.html"),
+            inject: "body"
+        }),
+        new MiniCssExtractPlugin({
+            filename: path.resolve(__dirname, 'build', 'css', 'style.css'),
+        }),
     ]
 };
